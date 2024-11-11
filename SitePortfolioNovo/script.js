ab0cb8ca041f959a040c14319be736b078a75d38
Mateus Costa <cpsmateus16@gmail.com>
2024-11-11 18:20:28 -0300

    Melhorando Portfolio Novo

--- SitePortfolioNovo/script.js
+++ SitePortfolioNovo/script.js
@@ -1,10 +1,6 @@
-// Validação e mensagem de confirmação no envio do formulário
 document.getElementById('formulario-contato').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita o envio automático
-
-    // Exibe a mensagem de confirmação
+    event.preventDefault(); 
     document.getElementById('mensagem-confirmacao').style.display = 'block';
 
-    // Limpa o formulário após o envio
     this.reset();
 });
